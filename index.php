15333c0b303852e1c5bb8e4dfcdfee51d0c0d62a
eruditmorina <erudit.morina@gmail.com>
2020-06-16 17:25:30 +0200

    Plus the BackToTop Button

--- index.php
+++ index.php
@@ -1,3 +1,17 @@
 <?php get_header();?>
-    <h1>INDEX</h1>
+
+    <!-- Back to top button -->
+    <a class="buttonToTop"></a>
+
+    <h1 id="test"></h1>
+
+    <script>
+        var text = "";
+        var i;
+        for (i = 0; i < 25; i++) {
+            text += "INDEX" + "<br>";
+        }
+        document.getElementById("test").innerHTML = text;       
+    </script>
+
 <?php get_footer();?>
